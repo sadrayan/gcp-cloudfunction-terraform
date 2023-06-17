f9c27e5656283bfdaf9b7307bd98c8b783e1af92
SABRISHA <sabrisha@ford.com>
2023-06-17 09:24:46 -0400

    helper scripts

---
+++ compile-upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "------------ building project -------"
+cd app/MyHTTPFunction
+npm run build:ci
+cd -
+
+git_sha=$(git rev-parse HEAD)
+sa="github-action-pipeline@sandbox-381502.iam.gserviceaccount.com"
+
+gcloud --quiet config set project sandbox-381502
+gcloud --quiet --impersonate-service-account=$sa alpha storage cp app/MyHTTPFunction/dist/my-http-function.zip gs://artifact-sandbox-381502/MyHTTPFunction/myfunction-"$git_sha".zip
+
---
+++ run-tf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "------------ Running Terraform  ------------------"
+git_sha=$(git rev-parse HEAD)
+
+cd terraform
+
+export GOOGLE_OAUTH_ACCESS_TOKEN=$(gcloud auth print-access-token --impersonate-service-account=github-action-pipeline@sandbox-381502.iam.gserviceaccount.com)
+
+terraform plan -var "revision=$git_sha" -var-file="dev/terraform.tfvars" -out tfplan
+
+terraform apply tfplan
